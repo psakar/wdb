59c5209a5a469a348a0ff89ab48bd6799fd20ab9
Petr Sakař <petr.sakar@chare.eu>
2021-04-25 17:04:34 +0200

    initial import

---
+++ src/test/java/org/chare/wdb/WdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chare.wdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
